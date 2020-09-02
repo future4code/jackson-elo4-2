11f3c8da28d5a357ec6e29ec7e7888f6bc731bed
luaanaapreiraa <luanapdsantos@gmail.com>
2020-09-02 16:55:42 -0300

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import {RedesSociais, Desenvolvedores}from "./styled.js";
+import Facebook from "../../Img/Facebook.jpg"
+import Instagram from "../../Img/Instagram.png";
+import Twitter from "../../Img/Twitter.png";
 
 class Footer extends React.Component {
   render(){
     return (
-    <div></div>
+    <div>
+    <RedesSociais>
+      
+
+        <p> Redes Sociais </p>
+        <img src={Facebook}
+        style={{
+          width: "5%", height: "50%",
+        }} />
+        <img src={Instagram} 
+        style={{
+          width: "7%", height: "50%",
+        }}
+        />
+        <img src={Twitter}
+        style={{
+          width: "5%", height:"50%",
+        }}
+        />
+     
+
+    </RedesSociais>
+  
+    <Desenvolvedores>
+      <p> <b> Desenvolvido por Turma Jackson</b> </p>
+    </Desenvolvedores>
+    </div>
     );
   };
 };
---
+++ src/components/Footer/styled.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+export const  RedesSociais = styled.div `
+display: flex;
+align-items: stretch;
+flex-wrap: wrap;
+border: 1px solid black;
+background-color: #f2d0a7;
+height: 100px;
+
+`
+export const Desenvolvedores = styled.div`
+display: flex;
+align-items: flex-end;
+align-items: stretch;
+flex-wrap: wrap;
+border: 1px solid black;
+background-color: #59d9d9;
+
+`
+export const Img = styled.div`
+width: 2px;
+`
+
+/*display: flex;
+justify-content:space-around;
+border: 1px solid #49A695;
+background-color: #D2657C;
+color: #031226;
+width: 100vw;
+font-family: 'Space Mono', monospace;
+`
+*/
