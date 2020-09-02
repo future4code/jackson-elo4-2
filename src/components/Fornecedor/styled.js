3738d7d33d34375bfeb7f7c231215b8ebf087731
Carolina Della Nina <carolirn@gmail.com>
2020-09-02 18:45:50 -0300

    header no componente fornecedores e formataçao básica

--- src/components/Fornecedor/styled.js
+++ src/components/Fornecedor/styled.js
@@ -5,4 +5,42 @@ export const DivFormulario = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 2%;
+  margin-top: 2%;
+  border:2px solid orange;
+  margin-left: 10%;
+  margin-right: 10%;
+`
+export const H1 = styled.h1`
+  font-size: 25px;
+  color: #F28C0F;
+`
+
+export const BotaoCadastrar = styled.button`
+  margin: 2%;
+`
+
+export const Rotulo = styled.label`
+  margin: 6px;
+`
+
+export const FormInput = styled.input`
+  width: 30vw;
+`
+
+
+export const HeaderContainer = styled.div`
+    > div{
+        display:flex;
+        width:80%;
+        margin: 10px auto;
+        justify-content:space-between;
+        > img{
+            width:50px;
+        }
+    }
+    border-top:2px solid orange;
+    height:70px;
+    box-shadow: 0 0 3px gray;
+
 `
