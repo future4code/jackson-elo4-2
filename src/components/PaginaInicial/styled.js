665a33a258cfc37209249ab5b0b71d5bc762dbf3
Alice Souza <alicepsouzaba1998@gmail.com>
2020-09-02 22:46:12 -0300

    Abri o componente card e fiz mais na página inicial

--- src/components/PaginaInicial/styled.js
+++ src/components/PaginaInicial/styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 export const CategoriasContainer = styled.div`
-  width: 300px;
+  width: 200px;
+  padding-left: 40px;
+  padding-top: 30px;
 `
 export const ContainerPaginaInicial = styled.div`
   display: grid;
-  grid-template-columns: ${(props) => props.carrinhoAberto ? '1fr 3fr 1fr' : '1fr 3fr'};
+  grid-template-columns: ${(props) => props.carrinhoAberto ? '0.7fr 3fr 1fr' : '0.7fr 3fr'};
 `
 export const CarrinhoContainer = styled.div`
   @media (max-width: 415px){
@@ -20,5 +22,15 @@ export const CarrinhoContainer = styled.div`
   }
 `
 export const CardsContainer = styled.div`
-  
+  background: #ebebeb; 
+  padding-left: 55px;
+`
+export const CardsColecao = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+export const HeaderCards = styled.div`
+  padding-top: 20px;
+  height: 50px;
+  text-align: center;
 `
