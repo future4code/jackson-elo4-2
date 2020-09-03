21f7639cf97ee753862bd17767d2937cebbbd512
unknown <tbrito495@gmail.com>
2020-09-03 10:27:36 -0700

    alteraçao pequena carrinho

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,24 +1,16 @@
-
 import React from 'react';
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 class Carrinho extends React.Component {
   render(){
     return (
-    <div carrinhoAberto = {this.props.carrinhoAberto}>
-        <h1>Carrinho teste</h1>
-        <ul>
+    <ul>
           <li>
             {this.props.quantidadeProduto}x{this.props.tituloProduto}
             <button
               onClick={this.props.clickDeletarProduto}>Deletar</button>
           </li>
-
-        </ul>
-
-      
-
-    </div>
+    </ul>
     );
   };
 };
--- src/components/Carrinho/styled.js
+++ src/components/Carrinho/styled.js
@@ -1,5 +1,2 @@
 import styled from 'styled-components';
 
-export const NavLateral = styled.div`
-
-`
