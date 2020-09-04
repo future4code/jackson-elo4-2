3fc0e8ba5259fab726819eed78c23bafadc82761
Alice Souza <alicepsouzaba1998@gmail.com>
2020-09-04 13:57:43 -0300

    ajustes

---
+++ src/components/ProdutoUnicoInicial/ProdutoUnicoInicial.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Container} from './styled';
+
+class ProdutoUnicoInicial extends React.Component {
+  render() {
+    return (
+      <Container>
+        <h1>{this.props.tituloProduto}</h1>
+        <div>
+          <img src={this.props.foto1}/>
+          <img src={this.props.foto2}/>
+          <img src={this.props.foto3}/>
+        </div>
+        <p>{this.props.descricaoProduto}</p>
+        <ul>
+          <h3>Método de pagamento:</h3>
+          <li>{this.props.metodoPag}</li>
+          <li>Em até {this.props.parcelas}x</li>
+        </ul>
+        <br></br>
+        <p>Categoria: {this.props.categoria}</p>
+      </Container>
+    );
+  };
+};
+export default ProdutoUnicoInicial;
