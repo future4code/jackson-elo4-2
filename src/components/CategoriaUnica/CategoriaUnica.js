89808bbaf29135068de559db7dabf017b616d82e
luaanaapreiraa <luanapdsantos@gmail.com>
2020-09-06 12:00:30 -0300

    Filtros e ordenação

--- src/components/CategoriaUnica/CategoriaUnica.js
+++ src/components/CategoriaUnica/CategoriaUnica.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import Carrinho from '../Carrinho/Carrinho'
+import React from 'react';
+import styled from 'styled-components';
+import Carrinho from '../Carrinho/Carrinho';
+import ReactDOM from 'react-dom';
+import Filtro from './Filtro.js';
+import {Filtros} from './styled.js';
+import Ordem from './Ordem.js';
+import Card from '../Card/Card.js';
 
+
+  
 export default class CategoriaUnica extends React.Component {
-    render() {
-      return (
-        <div>
+ render(){
+    return (
+      <div>
+        <Filtros>
+          <h3>Filtros:</h3>
+          <Filtro/>
+          <Ordem/>
+        </Filtros>
+        <Card/>  {/*   teste */}
 
-      <Carrinho/>
-          
+          <Carrinho/>
   
-        </div>
-      )
-    }
+      </div>
+    );
   }
-  
-
+}
---
+++ src/components/CategoriaUnica/styled.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Filtros = styled.div`
+  width: 250px;
+  height:300px;
+  padding-left: 40px;
+  padding-top: 30px;
+  border: 1px solid black;
+
+`
+export const Categoria = styled.div `
+  display: flex;
+  
+`
